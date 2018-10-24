eaa233365d55adfa70fa3b6ba7e843c048c797d6
ShawYun <414252986@qq.com>
2018-10-24 22:17:49 +0800

    first commit

---
+++ Image_Load_Show_write.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+import cv2
+# 读取图片
+img = cv2.imread("F:/Python/pythonWorkspace/ShawOpencvLearning/pythonOpencvLearning/lena.bmp")
+# 创建窗口并显示图片
+cv2.namedWindow("Image")
+cv2.imshow("Image", img)
+# 不添这一句，在IDLE中执行窗口直接无响应。在命令行中执行的话，则是一闪而过。
+cv2.waitKey(0)
+# 释放窗口
+cv2.destroyAllWindows() 
